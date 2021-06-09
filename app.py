e75daa9b2414ca2fdc8343775eb04338a3c2599c
VighneshS <vighneshsivaraman@gmail.com>
2021-06-09 15:53:48 -0500

    Added status endpoint 😋

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
+def user_directory():
     return render_template('index.html')
+
+
+@app.route('/status')
+def hello_world():
+    return "Hello World"
 
 
 if __name__ == '__main__':
