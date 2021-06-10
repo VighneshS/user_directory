c1bca7b7ff229ec0d4021ce12ff63cfeb0be876c
VighneshS <vighneshsivaraman@gmail.com>
2021-06-09 22:09:41 -0500

    Showed the list of all the users and their details 😇

--- app.py
+++ app.py
@@ -1,33 +1,76 @@
-from flask import Flask, render_template
-# import pyodbc
+from flask import Flask, render_template, request
+import os
+import mysql.connector
+from mysql.connector import errorcode
 
 app = Flask(__name__)
 
-# server = os.getenv('SERVER_NAME')
-# database = os.getenv('DB_NAME')
-# username = os.getenv('USERNAME')
-# password = os.getenv('PASSWORD')
-# port = os.getenv('PORT')
+server = os.environ['UD_HOST_NAME']
+database = os.environ['UD_DB_NAME']
+username = os.environ['UD_DB_USERNAME']
+password = os.environ['UD_DB_PASSWORD']
 
-# server = 'vighnesh-cloud-computing.database.windows.net'
-# database = 'cloud_computing'
-# username = 'vighnesh@vighnesh-cloud-computing'
-# password = 'kira#552888'
-# port = 1433
-# driver = '/opt/homebrew/etc/odbcinst.ini'
-#
-# with pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-#     with conn.cursor() as cursor:
-#         cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
-#         row = cursor.fetchone()
-#         while row:
-#             print (str(row[0]) + " " + str(row[1]))
-#             row = cursor.fetchone()
+SELECT_ALL_FROM_PEOPLE_QUERY = "SELECT * FROM people"
 
 
-@app.route('/')
-def user_directory():
-    return render_template('index.html')
+def getConnection():
+    global cnx
+    try:
+        cnx = mysql.connector.connect(user=username, password=password,
+                                      host=server,
+                                      database=database)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    return cnx
+
+
+def fetchAllData():
+    getConnection()
+    cursor = cnx.cursor()
+    cursor.execute(SELECT_ALL_FROM_PEOPLE_QUERY)
+    people = cursor.fetchall()
+    cnx.close()
+    return people
+
+
+def fetchBasedOnNameAndState(name, state):
+    SELECT_FROM_PEOPLE_BASED_ON_NAME_AND_STATE_QUERY = " SELECT * from people "
+    getConnection()
+    cursor = cnx.cursor()
+    if name or state:
+        SELECT_FROM_PEOPLE_BASED_ON_NAME_AND_STATE_QUERY += " Where "
+    if name:
+        SELECT_FROM_PEOPLE_BASED_ON_NAME_AND_STATE_QUERY += " Name LIKE " + "'" + name + "'"
+    if state and name:
+        SELECT_FROM_PEOPLE_BASED_ON_NAME_AND_STATE_QUERY += " OR "
+    if state:
+        SELECT_FROM_PEOPLE_BASED_ON_NAME_AND_STATE_QUERY += " State LIKE " + "'" + state + "'"
+    print(SELECT_FROM_PEOPLE_BASED_ON_NAME_AND_STATE_QUERY)
+    cursor.execute(SELECT_FROM_PEOPLE_BASED_ON_NAME_AND_STATE_QUERY)
+    people = cursor.fetchall()
+    cnx.close()
+    return people
+
+
+@app.route('/', methods=['GET', 'POST'])
+def search():
+    global data
+    print(request.method)
+    if request.method == "POST":
+        print('Here')
+        name = request.form['name']
+        state = request.form['state']
+        print(name, state)
+        data = fetchBasedOnNameAndState(name, state)
+    else:
+        print('Get')
+        data = fetchAllData()
+    return render_template('index.html', data=data)
 
 
 @app.route('/status')
---
+++ environment_vars.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+export UD_HOST_NAME=localhost
+export UD_DB_NAME=user_directory
+export UD_DB_USERNAME=root
+export UD_DB_PASSWORD=password
